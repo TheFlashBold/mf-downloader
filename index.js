bdc2ac2250b54d1a2c20b4eb71dd919c0f165e03
tycho <tycho.holzer@gmail.com>
2020-04-09 15:45:56 +0200

    fixes

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const puppeteer = require('puppeteer-extra');
+
+puppeteer.use(StealthPlugin());
+
 module.exports = {
     module: require('./lib/DownloaderModule'),
-    DownloadPipeline: require('./lib/DownlodPipeline')
+    DownloadPipeline: require('./lib/DownlodPipeline'),
+    MiniSearch: require('minisearch'),
+    puppeteer: puppeteer
 };
